efaa497e5487c8a3b19ca8a71e1b5c5c8fc58e31
Mosh Hamedani <moshfegh@live.com.au>
2017-07-20 11:03:45 -0700

    .

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -10,11 +10,7 @@ import { Component } from '@angular/core';
 
       state('void', style({ opacity: 0 })),
 
-      transition('void => *', [
-        animate(2000)
-      ]),
-
-      transition('* => void', [
+      transition(':enter, :leave', [ 
         animate(2000)
       ])
     ])

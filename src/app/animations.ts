19cdf470e6b6225975cc06262258a9db2b8fa84e
Mosh Hamedani <moshfegh@live.com.au>
2017-07-20 12:31:47 -0700

    .

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -7,7 +7,7 @@ export let slide = trigger('slide', [
   ]),
 
   transition(':leave', [
-    animate(500, style({ transform: 'translateX(-100%)' }))
+    animate('0.5s ease-out', style({ transform: 'translateX(-100%)' }))
   ])
 ]);
 
